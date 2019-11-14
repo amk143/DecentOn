f238cce0e344f2bb33f1005a927695c8512fd650
amk143 <amoidkhan66@gmail.com>
2019-11-14 19:50:18 +0500

    Set Pictures, Butto, etc..

--- wp-content/themes/Divi/footer.php
+++ wp-content/themes/Divi/footer.php
@@ -37,19 +37,19 @@ if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
 
 			<?php endif; ?>
 
-				<div id="footer-bottom">
+				<!--<div id="footer-bottom">
 					<div class="container clearfix">
-				<?php
-					if ( false !== et_get_option( 'show_footer_social_icons', true ) ) {
-						get_template_part( 'includes/social_icons', 'footer' );
-					}
+				// <?php
+					// if ( false !== et_get_option( 'show_footer_social_icons', true ) ) {
+						// get_template_part( 'includes/social_icons', 'footer' );
+					// }
 
 					// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
-					echo et_core_fix_unclosed_html_tags( et_core_esc_previously( et_get_footer_credits() ) );
+					// echo et_core_fix_unclosed_html_tags( et_core_esc_previously( et_get_footer_credits() ) );
 					// phpcs:enable
 				?>
 					</div>	<!-- .container -->
-				</div>
+				<!--</div>-->
 			</footer> <!-- #main-footer -->
 		</div> <!-- #et-main-area -->
 
